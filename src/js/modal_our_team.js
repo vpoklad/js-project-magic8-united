86b0a5f2eaf8c64dd5f93f8aa980368d99f1cc2b
Olha Tsyhanenko <olya.tsyganenko@gmail.com>
2021-09-15 19:16:03 +0300

    up modal our team

--- src/js/modal_our_team.js
+++ src/js/modal_our_team.js
@@ -1,34 +1,52 @@
 const modalOurTeam = document.querySelector('[data-modal-our-team]');
-const backDrop = document.querySelector('.backdrop-our-team');
+const backDropOurTeam = document.querySelector('.backdrop-our-team__overlay');
 const btnOurTeamClose = document.querySelector('[data-modal-close]');
 const btnOurTeamOpen = document.querySelector('[data-modal-open]');
+const logo = modalOurTeam.querySelector('.logo-goit');
 
-
-
-console.log(modalOurTeam);
-console.log(btnOurTeamClose);
-console.log(backDrop);
 
 btnOurTeamOpen.addEventListener('click', onOpen);
-btnOurTeamClose.addEventListener('click', onClose);
-backDrop.addEventListener('click', onClose);
+btnOurTeamClose.addEventListener('click', onCloseBtn);
+backDropOurTeam.addEventListener('click', onClose);
 window.addEventListener('keydown', onClose);
 
 function onOpen(e) {
     e.preventDefault();
-    backDrop.classList.add('is-open');
+
+    modalOurTeam.classList.add('is-open');
+    document.body.style.overflow = 'hidden';
+    logo.classList.add('logo-goit-animate');
+}
+
+function onCloseBtn(e) {
+    e.preventDefault();
+    const currentActive = document.querySelector('.backdrop-our-team.is-open');
+    if (currentActive) {
+        currentActive.classList.remove('is-open');
+        document.body.style.overflow = '';
+        logo.classList.remove('logo-goit-animate');
+    }
 }
 
 function onClose(e) {
     e.preventDefault();
-
-    const currentActive = document.querySelector('.backdrop-our-team.is-open');
-
-    if (currentActive) {
-     currentActive.classList.remove('is-open');   
-    } else if ((!e.target.classList.contains('backdrop-our-team')) || (e.key !== "Escape")) {
+const currentActive = document.querySelector('.backdrop-our-team.is-open');
+    if ((e.key !== "Escape") && (!e.target.classList.contains('backdrop-our-team__overlay'))){
         return;
-    } 
-     currentActive.classList.remove('is-open');       
+    }
+    
+    currentActive.classList.remove('is-open');
+    document.body.style.overflow = '';
+    logo.classList.remove('logo-goit-animate');
+    
 }
+
+
+    // if (currentActive) {
+    //  currentActive.classList.remove('is-open');   
+    // } else if ((!e.target.classList.contains('backdrop-our-team')) || (e.key !== "Escape")) {
+    //     return;
+    // } 
+    //  currentActive.classList.remove('is-open');       
+
 
