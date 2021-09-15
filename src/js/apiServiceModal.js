4e2aacda9491bc9ec0102469f2fb8f27f1c91764
khomasl <gymnasium.yv@gmail.com>
2021-09-15 17:24:03 +0300

    фиксы после мержа

--- src/js/apiServiceModal.js
+++ src/js/apiServiceModal.js
@@ -7,14 +7,14 @@ const API_KEY  = '7elxdku9GGG5k8j0Xm8KWdANDgecHMV0';
 
 export default class EventApiServiceById {
     constructor() {
-        this.eventId = '';
+        this.id = '';
         this.page = 1;
         this.size = 20;
     }
 
     async fetchEvent () {
         //const url = `${ROOT_URL}&q=${this.searchQuery}&page=${this.page}&per_page=${this.perPage}&apikey=${API_KEY}`;
-        const url = `${ROOT_URL}/${this.eventId}?apikey=${API_KEY}`;
+        const url = `${ROOT_URL}/${this.id}?apikey=${API_KEY}`;
         try {
             const result = await axios.get(url);
             console.log('result :>> ', result);
@@ -34,10 +34,10 @@ export default class EventApiServiceById {
     }
 
     get query() {
-        return this.eventId;
+        return this.id;
     }
 
     set query (newId) {
-        this.eventId = newId;
+        this.id = newId;
     }
 }
