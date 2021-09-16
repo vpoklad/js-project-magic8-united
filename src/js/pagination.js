ccd437d6141cebb795bfd78fcfd9f4d58c977ef3
Anidamp <79271758+Anidamp@users.noreply.github.com>
2021-09-16 19:53:43 +0300

    pagin

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,29 +1,37 @@
 import Pagination from 'tui-pagination'; 
+import eventServiceApi from "./search-API"
+import {renderEventMarkup} from './renderEventIntoModal'
+
 
 const container = document.querySelector('#tui-pagination-container');
-const options = { // below default value of options
-     totalItems: 100,
-     itemsPerPage: 16,
-     visiblePages: 5,
-     page: 1,
-     centerAlign: true,
-     firstItemClassName: 'tui-first-child',
-     lastItemClassName: 'tui-last-child',
-     template: {
-         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
-         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-         moveButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}">' +
-                 '<svg class="tui-ico-{{type}}"><use xlink:href="./images/icon-{{type}}.svg"></use></svg>' +
-             '</a>',
-         disabledMoveButton:
-             '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-                 '<svg class="tui-ico-{{type}}"><use href="./images/icon-{{type}}.svg"></use></use></svg>' +
-             '</span>',
-         moreButton:
-             '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
-                 '<span class="tui-ico-ellip">...</span>' +
-             '</a>'
-     }
-};
-const pagination = new Pagination(container, options);
+
+function setEventsOnPage() {
+    const windowOuterWidth = window.outerWidth;
+    if (windowOuterWidth > 768 && windowOuterWidth < 1280) {
+      apiService.size = 15;
+    } else {
+      apiService.size = 16;
+    }
+  }
+
+
+
+function setPagination(totalEvents) {
+    const options = {
+        totalItems: totalEvents > 1000 ? 1000 : totalEvents,
+        itemsPerPage: eventServiceApi.size,
+        visablePages: 5,
+        page: 1,
+        centerAlign: true,
+
+    };
+    const pagination = new Pagination(container, options);
+
+    pagination.on('beforeMove', function (eventData) {
+        apiService.page = eventData.page - 1;
+        setEventsOnPage();
+        apiService.fetchEvent().then(renderEventMarkup).catch(console.log);
+      });
+}
+
+export default setPagination;
