cd1d822929f849458d47da4d156fd81afef247fe
SvitlanaDmytriv <svetagawrylyuk26@gmail.com>
2021-09-16 14:33:31 +0300

    addAnimationEvents

--- src/js/firstPageLoad.js
+++ src/js/firstPageLoad.js
@@ -1,16 +1,33 @@
 import axios from "axios";
 import refs from './refs.js'
 import card from '../templates/card.hbs';
+import { debounce } from "lodash";
 
 
 function firstPageLoad() {
-      axios.get('https://app.ticketmaster.com/discovery/v2/events.json?&classificationName=music&apikey=y2gr3zDEoAnck6YziFkTdrHptQULpZRO')
-        .then(result => {
-          renderCards(result.data._embedded.events);
-        console.log(result.data._embedded.events)}).catch(err => console.log(err))
+  axios.get('https://app.ticketmaster.com/discovery/v2/events.json?&classificationName=music&apikey=y2gr3zDEoAnck6YziFkTdrHptQULpZRO')
+    .then(result => {
+      renderCards(result.data._embedded.events)
+      addClassAnimation()
+    }).catch(err => console.log(err))
+  
   }
 
 
+function addClassAnimation() {
+ const cardsItemAll = document.querySelectorAll('.cards__item')
+  cardsItemAll.forEach(cardItem => cardItem.classList.add('cards__item--animation'))
+}
+
+function removeClassAnimation() {
+ const cardsItemAll= document.querySelectorAll('.cards__item')
+  cardsItemAll.forEach(cardItem => {
+  cardItem.classList.remove('cards__item--animation')
+  cardItem.style.visibility = "visible"
+  })
+  
+
+}
   
 function renderCards(events) {
     const markup = card(events)
@@ -19,3 +36,4 @@ function renderCards(events) {
 }
 
 document.addEventListener("DOMContentLoaded", firstPageLoad);
+window.addEventListener("load",debounce(removeClassAnimation,500))
