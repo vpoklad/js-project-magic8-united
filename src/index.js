ab3cefebdc725998bce5eaed49f403608e51711c
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-09-16 18:23:46 +0300

    add more from this author script

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import './sass/main.scss';
 import CustomSelect from './js/custom-select';
 import './js/event-search';
-import './js/select'
+import './js/select';
 import './js/modalClose';
 import './js/firstPageLoad.js';
 import './js/pagination';
 import './js/modal_our_team';
 import './js/renderEventIntoModal.js';
-
-
+import './js/morefromauthor.js';
