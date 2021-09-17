ccfe697ed8eeba8385f59f8e835c6c3fda125642
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-09-18 00:54:20 +0300

    init feature-mfta_002

--- src/js/morefromauthor.js
+++ src/js/morefromauthor.js
@@ -1,11 +1,17 @@
 import refs from './refs.js';
 import onCloseMainModal from './modalClose.js';
+import EventServiceApi from './search-API';
+import templateCard from '../templates/card.hbs';
 
 refs.buttonMoreFromAuthor.addEventListener('click', searchMoreFromAuthor);
 
 function searchMoreFromAuthor() {
   onCloseMainModal();
   refs.cardsContainer.innerHTML = '';
-  //обнулить пагинацию
-  //вызавать поиск
+  refs.searchInput.value = '0';
+  const eventServiceApi = new EventServiceApi();
+  eventServiceApi.query = refs.searchInput.value;
+  eventServiceApi.fetchEvent().then(response => {
+    refs.cardsContainer.innerHTML = templateCard(response);
+  });
 }
--- src/js/search-API.js
+++ src/js/search-API.js
@@ -1,74 +1,83 @@
 import axios from 'axios';
-import { alert, notice, info, success, error } from '../../node_modules/@pnotify/core/dist/PNotify.js';
+import {
+  alert,
+  notice,
+  info,
+  success,
+  error,
+} from '../../node_modules/@pnotify/core/dist/PNotify.js';
 import '@pnotify/core/dist/BrightTheme.css';
 
-const ROOT_URL = "https://app.ticketmaster.com/discovery/v2/";
-const KEY = "y2gr3zDEoAnck6YziFkTdrHptQULpZRO";
-
+const ROOT_URL = 'https://app.ticketmaster.com/discovery/v2/events';
+//const ROOT_URL = 'https://app.ticketmaster.com/discovery/v2/attractions';
+//const ROOT_URL = 'https://app.ticketmaster.com/discovery/v2/classifications';
+//const ROOT_URL = 'https://app.ticketmaster.com/discovery/v2/venues';
+const KEY = 'y2gr3zDEoAnck6YziFkTdrHptQULpZRO';
 
 export default class EventServiceApi {
   constructor() {
     this.page = 1;
-    this.totalPages ='';
-    this.searchQuery = "";
-    this.countryQuery = "";
-    this.eventId = "";
+    this.totalPages = '';
+    this.searchQuery = '';
+    this.countryQuery = '';
+    this.eventId = '';
   }
 
   fetchEvent() {
-    let url = `${ROOT_URL}events.json?&page=${this.page}&keyword=${this.searchQuery}&apikey=${KEY}`;
-    
+    let url = `${ROOT_URL}.json?&page=${this.page}&keyword=${this.searchQuery}&apikey=${KEY}`;
+
     if (this.countryQuery !== '') {
-      url =`${ROOT_URL}events.json?&page=${this.page}&countryCode=${this.countryQuery}&keyword=${this.searchQuery}&apikey=${KEY}`;
+      url = `${ROOT_URL}.json?&page=${this.page}&countryCode=${this.countryQuery}&keyword=${this.searchQuery}&apikey=${KEY}`;
     }
-    
-    return axios.get(url).then(result => {
-     
-       if (!result.data._embedded) {
-        alert({
-          text: "Looks like there is no such even!",
-          delay: 2000,
-        })
-        // console.log("Looks like there is no such even!");
-        return
-      }
-      
-      try {
-        // this.page += 1;
-        // console.log(result.data);
-        this.totalPages = result.data.page.totalPages;
-        // console.log(this.totalPages);
-        return result.data._embedded.events;
-      } catch (error) {
-        // Поставить Нотификашку для отлова ошибки
-        console.log("try catch", error);
-      }
-     
-    }).catch(err => {
-      console.log('catch', err)
-    }) 
+
+    return axios
+      .get(url)
+      .then(result => {
+        if (!result.data._embedded) {
+          alert({
+            text: 'Looks like there is no such even!',
+            delay: 2000,
+          });
+          // console.log("Looks like there is no such even!");
+          return;
+        }
+
+        try {
+          // this.page += 1;
+          // console.log(result.data);
+          this.totalPages = result.data.page.totalPages;
+          // console.log(this.totalPages);
+          return result.data._embedded.events;
+        } catch (error) {
+          // Поставить Нотификашку для отлова ошибки
+          console.log('try catch', error);
+        }
+      })
+      .catch(err => {
+        console.log('catch', err);
+      });
   }
 
   searchEventById() {
-    return axios.get(`${ROOT_URL}events/${this.eventId}.json?apikey=${KEY}`).then(data => {
+    return axios.get(`${ROOT_URL}/${this.eventId}.json?apikey=${KEY}`).then(data => {
       console.log(data);
-      return data
-    })
+      return data;
+    });
   }
 
-  pageReset(){
-      this.page = 1;
+  pageReset() {
+    this.page = 1;
   }
   incrementPage() {
     this.page += 1;
   }
   decrementPage() {
     if (this.page === 1) {
-      return
+      return;
     }
     this.page -= 1;
-   }
-  
+  }
+
   get query() {
     return this.searchQuery;
   }
