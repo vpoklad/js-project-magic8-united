8ed4c649341addda5c112fc5ba930e2ec708d3fa
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-09-18 19:34:45 +0300

    init feature-mfta_002

--- src/js/morefromauthor.js
+++ src/js/morefromauthor.js
@@ -2,15 +2,18 @@ import refs from './refs.js';
 import onCloseMainModal from './modalClose.js';
 import EventServiceApi from './search-API';
 import templateCard from '../templates/card.hbs';
+import { authorName } from './renderEventIntoModal.js';
 
 refs.buttonMoreFromAuthor.addEventListener('click', searchMoreFromAuthor);
+console.log(authorName);
 
 function searchMoreFromAuthor() {
   onCloseMainModal();
   refs.cardsContainer.innerHTML = '';
-  refs.searchInput.value = '0';
+  refs.searchInput.value = '';
+  refs.searchInput.value = authorName;
   const eventServiceApi = new EventServiceApi();
-  eventServiceApi.query = refs.searchInput.value;
+  eventServiceApi.query = refs.searchInput.value.replace(' ', '%20');
   eventServiceApi.fetchEvent().then(response => {
     refs.cardsContainer.innerHTML = templateCard(response);
   });
